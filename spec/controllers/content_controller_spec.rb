54bb539062531dec8aea0ce6155825a88fe319b1
Spencer Dixon <spencerlloyddixon@gmail.com>
2025-06-06 11:25:08 +0100

    Added base layout, article layout and tests

---
+++ spec/controllers/content_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe ContentController, type: :controller do
+  describe "GET #show" do
+    let(:slug) { "test" }
+    let(:front_matter) { {} }
+    let(:markdown_content) { "# Hello World" }
+
+    before do
+      allow(ContentLoader.instance).to receive(:find_by_slug)
+        .with(slug)
+        .and_return([ front_matter, markdown_content ])
+    end
+
+    context "when layout is specified in the front matter" do
+      let(:front_matter) { { layout: "article" } }
+
+      it "uses the layout specified in front matter" do
+        get :show, params: { slug: slug }
+
+        expect(controller.send(:set_layout)).to eq("article")
+      end
+    end
+
+    context "when layout is not specified" do
+      it "defaults to application layout" do
+        get :show, params: { slug: slug }
+
+        expect(controller.send(:set_layout)).to eq("application")
+      end
+    end
+  end
+end
