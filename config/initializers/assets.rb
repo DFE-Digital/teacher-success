08518ebee7209309ad721e7e94eef9883d36f1b5
Spencer Dixon <spencerlloyddixon@gmail.com>
2025-06-06 11:50:13 +0100

    Add node build steps to ci

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,3 @@
 Rails.application.config.assets.paths << Rails.root.join("node_modules/govuk-frontend/dist/govuk/assets")
 Rails.application.config.assets.paths << Rails.root.join("node_modules/dfe-frontend/packages/assets")
+Rails.application.config.assets.paths << Rails.root.join("app/assets/builds")
