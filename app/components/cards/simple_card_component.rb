ab20fdb6dc57cc5363e4972c6aaaf6b17cdf2fb8
Spencer Dixon <spencerlloyddixon@gmail.com>
2025-06-23 14:27:39 +0100

    Add custom heading_tag to simple card

--- app/components/cards/simple_card_component.rb
+++ app/components/cards/simple_card_component.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class Cards::SimpleCardComponent < ViewComponent::Base
-  def initialize(title:, description:, path:)
+  def initialize(title:, description:, path:, heading_tag: "h2")
     @title = title
     @description = description
     @path = path
+    @heading_tag = heading_tag
   end
 end
