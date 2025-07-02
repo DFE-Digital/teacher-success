fd9d9bbe1d3786bdb9eed7bcc95971a2fb6297da
Spencer Dixon <spencerlloyddixon@gmail.com>
2025-07-02 11:07:40 +0100

    Add solid queue

---
+++ app/controllers/admin/mission_control_controller.rb
@@ -0,0 +1,6 @@
+class Admin::MissionControlController < ApplicationController
+  http_basic_authenticate_with(
+    name: ENV["MISSION_CONTROL_NAME"],
+    password: ENV["MISSION_CONTROL_PASSWORD"]
+  )
+end
