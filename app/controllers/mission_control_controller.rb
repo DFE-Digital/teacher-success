01cd67a7c0c090ea82678c59868223ae729a5fb2
Spencer Dixon <spencerlloyddixon@gmail.com>
2025-07-07 12:14:23 +0100

    Allow mission control to be accessed in development

--- app/controllers/mission_control_controller.rb
+++ app/controllers/mission_control_controller.rb
@@ -2,5 +2,5 @@ class MissionControlController < ApplicationController
   http_basic_authenticate_with(
     name: ENV["BASIC_AUTH_USERNAME"],
     password: ENV["BASIC_AUTH_PASSWORD"]
-  )
+  ) unless Rails.env.development?
 end
