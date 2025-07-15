8f053f86b48c1d5eaf3b22325c245d9a88555747
Spencer Dixon <spencerlloyddixon@gmail.com>
2025-07-15 16:10:56 +0100

    Lint

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -5,7 +5,7 @@ module MarkdownHelper
       type: :erb,
       variants: lookup_context.variants,
       formats:  lookup_context.formats
-    ).gsub(/>\s*\n\s*</, '><') # Remove newlines and spaces between adjacent HTML tags as these can get rendered to <p> tags in the markdown parser
+    ).gsub(/>\s*\n\s*</, "><") # Remove newlines and spaces between adjacent HTML tags as these can get rendered to <p> tags in the markdown parser
 
     GovukMarkdown.render(evaluated).html_safe
   end
