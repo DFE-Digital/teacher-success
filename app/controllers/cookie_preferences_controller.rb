e84c3afa55d9c253051ee7362892a06f2f7d3908
Spencer Dixon <spencerlloyddixon@gmail.com>
2025-07-18 10:50:48 +0100

    Conditionally show the flash on the cookie_preferences/edit page only

--- app/controllers/cookie_preferences_controller.rb
+++ app/controllers/cookie_preferences_controller.rb
@@ -14,13 +14,17 @@ class CookiePreferencesController < ApplicationController
       non_essential: ActiveRecord::Type::Boolean.new.deserialize(non_essential_preference)
     })
 
-    flash = {
-      title: "Success",
-      heading: "Cookie Preferences Updated",
-      description: "Your cookie preferences have been saved."
-    }
+    if URI(request.referer || "").path == edit_cookie_preferences_path 
+      # Only set a flash if we're updating from the cookie_preferences/edit page
+      # If we're coming from the home page, dismissing the banner is enough
+      flash[:success] = {
+        title: "Success",
+        heading: "Cookie Preferences Updated",
+        description: "Your cookie preferences have been saved."
+      }
+    end
 
-    redirect_to edit_cookie_preferences_path, flash: { success: flash }
+    redirect_to request.referer || root_path
   end
 
   private
