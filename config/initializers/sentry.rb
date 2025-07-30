0b7bcaeff43e5982ccd173cf908bd53006de7159
Spencer Dixon <spencerlloyddixon@gmail.com>
2025-07-30 11:41:36 +0100

    Add logs to sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,4 +4,6 @@ Sentry.init do |config|
   config.breadcrumbs_logger = [ :active_support_logger ]
   config.dsn = ENV["SENTRY_DSN"]
   config.traces_sample_rate = 1.0
+  config.enable_logs = true
+  config.enabled_patches = [:logger]
 end
