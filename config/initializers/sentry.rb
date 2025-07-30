060bcfe98f513115d69b1429d23097c503d985e5
Spencer Dixon <spencerlloyddixon@gmail.com>
2025-07-30 12:02:12 +0100

    Lint

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,5 +5,5 @@ Sentry.init do |config|
   config.dsn = ENV["SENTRY_DSN"]
   config.traces_sample_rate = 1.0
   config.enable_logs = true
-  config.enabled_patches = [:logger]
+  config.enabled_patches = [ :logger ]
 end
