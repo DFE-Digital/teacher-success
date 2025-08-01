b885cda2779b1ef42df49c1764c97361643440d3
alexleedfe <115729523+alexleedfe@users.noreply.github.com>
2025-08-01 11:57:42 +0000

    adding nil instead of empty ""

--- app/components/cards/quote_component.rb
+++ app/components/cards/quote_component.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Cards::QuoteComponent < ViewComponent::Base
-  def initialize(text:, attribution:, classes: "")
+  def initialize(text:, attribution:, classes: "nil")
     @text = text
     @attribution = attribution
     @classes = classes
