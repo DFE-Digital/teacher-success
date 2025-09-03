969d0764b89e78841027742e48fc0e833a88560d
Jamie <jamiecleare2525@hotmail.com>
2025-09-03 17:31:27 +0100

    Prompt component

---
+++ app/components/cards/prompt_component.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class Cards::PromptComponent < ViewComponent::Base
+  def initialize(prompt_type: :info, classes: nil)
+    @prompt_type = prompt_type
+    @classes = classes
+  end
+
+  def icon_attributes
+    if @prompt_type == :info
+      {
+        name: "fa-circle-info",
+        data: "circle-info",
+        draw: "M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM216 336l24 0 0-64-24 0c-13.3 0-24-10.7-24-24s10.7-24 24-24l48 0c13.3 0 24 10.7 24 24l0 88 8 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-80 0c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-208a32 32 0 1 1 0 64 32 32 0 1 1 0-64z",
+      }
+    elsif [:warning, :error].include?(@prompt_type) # unused icon
+      {
+        name: "fa-triangle-exclamation",
+        data: "triangle-exclamation",
+        draw: "M256 0c14.7 0 28.2 8.1 35.2 21l216 400c6.7 12.4 6.4 27.4-.8 39.5S486.1 480 472 480L40 480c-14.1 0-27.2-7.4-34.4-19.5s-7.5-27.1-.8-39.5l216-400c7-12.9 20.5-21 35.2-21zm0 352a32 32 0 1 0 0 64 32 32 0 1 0 0-64zm0-192c-18.2 0-32.7 15.5-31.4 33.7l7.4 104c.9 12.5 11.4 22.3 23.9 22.3 12.6 0 23-9.7 23.9-22.3l7.4-104c1.3-18.2-13.1-33.7-31.4-33.7z",
+      }
+    end
+  end
+end
