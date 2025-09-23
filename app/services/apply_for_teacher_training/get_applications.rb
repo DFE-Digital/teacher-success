728cf9ff17f120793c8460008862f8e789145110
Jamie <jamiecleare2525@hotmail.com>
2025-09-23 17:16:24 +0100

    OneLogin integration

--- app/services/apply_for_teacher_training/get_applications.rb
+++
@@ -1,7 +0,0 @@
-module ApplyForTeacherTraining
-  class GetApplications
-    def call
-      HTTParty.get(ENV.fetch('APPLICATION_API_URL'))
-    end
-  end
-end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -8,31 +8,29 @@ when "persona"
       :developer,
       fields: %i[uid email first_name last_name],
       uid_field: :uid,
-      )
+    )
   end
 
 when "dfe-sign-in"
-  dfe_sign_in_issuer_uri = URI(ENV.fetch("DFE_SIGN_IN_ISSUER_URL"))
-  dfe_sign_in_redirect_uri = "#{ENV.fetch("APP_BASE_URL")}/auth/dfe/callback"
-
   Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :openid_connect, {
-      name: :dfe,
-      discovery: true,
-      scope: %i[email profile],
-      response_type: :code,
-      path_prefix: "/auth",
-      callback_path: "/auth/dfe/callback",
-      issuer: "#{dfe_sign_in_issuer_uri}",
-
-      client_options: {
-        port: dfe_sign_in_issuer_uri.port,
-        scheme: dfe_sign_in_issuer_uri.scheme,
-        host: dfe_sign_in_issuer_uri.host,
-        identifier: ENV.fetch("DFE_SIGN_IN_CLIENT_ID"),
-        secret: ENV.fetch("DFE_SIGN_IN_SECRET"),
-        redirect_uri: dfe_sign_in_redirect_uri
-      }
+    private_key = ENV["GOVUK_ONE_LOGIN_PRIVATE_KEY"].gsub("\\n", "\n")
+    provider :govuk_one_login, {
+      name: :govuk_one_login,
+      client_id: ENV["GOVUK_ONE_LOGIN_CLIENT_ID"], # your client ID from the GOV.UK One Login admin tool
+      idp_base_url: ENV["GOVUK_ONE_LOGIN_BASE_URL"],
+      private_key: OpenSSL::PKey::RSA.new(private_key), # the private key you generated above in PEM format
+      redirect_uri: ENV["GOVUK_ONE_LOGIN_REDIRECT_URI"], # if this is a relative URI, the requesting domain will be used
+      # these are optional - shown here with their default values if omitted
+      private_key_kid: "", # the key ID of the private key being used - if using a JWKS endpoint, this must be set for authorization to work
+      scope: "openid,email", # comma-separated; must include at least `openid` and `email`
+      ui_locales: "en", # comma-separated; can also include `cy` for Welsh UI
+      vtr: ["Cl.Cm"], # array with one element; dot-separated; can also include identity vectors such as `P2` (eg. `Cl.Cm.P2`)
+      pkce: false, # set to `true` to enable "Proof Key for Code Exchange"
+      userinfo_claims: [], # array of URLs; see https://docs.sign-in.service.gov.uk/integrate-with-integration-environment/authenticate-your-user/#create-a-url-encoded-json-object-for-lt-claims-request-gt
+      signing_algorithm: 'ES256', # The algorithm used to encode/decode the JWT token, RS256 also supported
     }
+
+    # will call `Users::OmniauthController#failure` if there are any errors during the login process
+    on_failure { |env| SessionsController.action(:failure).call(env) }
   end
 end
