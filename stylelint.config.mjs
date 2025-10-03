80ce9b64ba802e92c8cb84f5c14f5a888fd4749b
Spencer Dixon <spencerlloyddixon@gmail.com>
2025-10-03 16:49:17 +0100

    Fix styles to adhere to stylelint

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import("stylelint").Config} */
 export default {
-  "extends": ["stylelint-config-gds/scss"]
+  "extends": ["stylelint-config-gds/scss"],
+  "rules": {
+    "max-nesting-depth": 4,
+    "selector-no-qualifying-type": [true, { ignore: ["class", "attribute"] }],
+    "color-no-hex": true, // We should set colours as variables, not use explicit hex colours
+    "selector-max-id": 1
+  }
 };
